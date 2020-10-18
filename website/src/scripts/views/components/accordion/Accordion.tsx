c3d747bcf2c75d9580fd938af523387b1fb4b879
dBanasiak <damian.banasiak96@gmail.com>
2020-10-19 00:51:06 +0200

    SETUP: add styled components - add sass

--- website/src/scripts/views/components/accordion/Accordion.tsx
+++ website/src/scripts/views/components/accordion/Accordion.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { accordionActions } from '../../../state/ducks/accordion';
 import { getIsAccordionOpen } from '../../../state/ducks/accordion/selectors';
+import { AccordionCTA, AccordionWrapper } from '../../styles/styledAccordion/StyledAccordion';
 
 const mapStateToProps = (state) => ({
   isOpen: getIsAccordionOpen(state),
@@ -13,11 +14,11 @@ const actionCreators = {
 
 const Accordion = ({ isOpen, toggleAccordion, ctaLabel }) => (
   <div>
-    <button onClick={toggleAccordion}>
+    <AccordionCTA onClick={toggleAccordion}>
       {ctaLabel}
       {isOpen}
-    </button>
-    {isOpen && <div style={{ color: 'red', width: '100px', height: '100px' }}>Im totally open</div>}
+    </AccordionCTA>
+    {isOpen && <AccordionWrapper>Im totally open</AccordionWrapper>}
   </div>
 );
 
